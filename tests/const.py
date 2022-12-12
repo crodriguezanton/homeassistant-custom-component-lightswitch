74dc660cad36deeed70dbb88d97a41ce4c3b931c
Carlos Rodríguez <git@carlos.tech>
2022-12-12 20:55:56 +0100

    chore: initial commit

---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Lightswitch tests."""
+from custom_components.lightswitch.const import (
+    CONF_PASSWORD,
+)
+from custom_components.lightswitch.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
